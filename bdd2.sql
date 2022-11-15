45d1ac7646363a8a60d498f37ba8542956339cdc
Alejandro Alvarez <zorro-cristal@hotmail.com>
2022-11-15 18:28:58 -0300

    Base de datos para autenticaciones agregada

---
+++ bdd2.sql
@@ -0,0 +1,18 @@
+CREATE TABLE usuarios (
+	alias VARCHAR(50) NOT NULL,
+	contra BLOB,
+	rol_id INT (9) NOT NULL,
+	PRIMARY KEY (alias)
+);
+
+CREATE TABLE roles (
+	id INT(9) AUTO_INCREMENT,
+	nombre VARCHAR(20),
+	PRIMARY KEY (id)
+);
+
+ALTER TABLE usuarios ADD FOREIGN KEY (rol_id) REFERENCES roles(id);
+
+INSERT INTO roles(nombre) VALUES ("administrador");
+
+INSERT INTO usuarios(alias, contra, rol_id) VALUES ("zorro-cristal", AES_ENCRYPT("zorro-cristal", "inventario"), 1);
