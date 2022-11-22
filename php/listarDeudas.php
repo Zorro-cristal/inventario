f280e239e452dac92346c1ee443306d98fe54708
Alejandro Alvarez <zorro-cristal@hotmail.com>
2022-11-22 20:29:26 -0300

    Limpieza de archivos

--- php/listarDeudas.php
+++
@@ -1,6 +0,0 @@
-<?php
-    include './funcionesbdd.php';
-    $clientes= conectarBdd("SELECT * FROM clientes WHERE (deuda > 0)");
-    $aux= json_encode($clientes);
-    echo ($aux);
-?>
--- php/saldarDeuda.php
+++
@@ -1,9 +0,0 @@
-<?php
-    include './funcionesbdd.php';
-    $pago= $_POST['pago'];
-    $deuda= $_POST['deuda'];
-    $cedula= $_POST['cedula'];
-    $comando= "UPDATE clientes SET deuda= " . $deuda - $pago . " WHERE cedula=" . $cedula;
-    modificarBdd($comando);
-    header('Location: ../pages/listarDeudas.html');
-?>
