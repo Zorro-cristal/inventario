aa4beed8c42526c917f8d98be1b5146b2e2bf891
Alejandro Alvarez <zorro-cristal@hotmail.com>
2022-12-01 17:17:19 -0300

    Paginas de timbrado agregados

---
+++ php/agregar_timbrado.php
@@ -0,0 +1,19 @@
+<?php
+    include './funcionesbdd.php';
+    // Obtenemos los datos del timbrado
+    $cod= $_POST['timbrado'];
+    $fech_auto= $_POST['fech_autoriz'];
+    $fech_venc= $_POST['fech_venc'];
+    $fech_auto= convertirFecha($fech_auto);
+    $fech_venc= convertirFecha($fech_venc);
+    // Eliminamos si existe un timbrado que vensa antes
+    $sql= "DELETE FROM timbrados WHERE fech_vencimiento > " . $fech_venc;
+    modificarBdd($sql);
+    // Guardamos el nuevo timbrado
+    $sql= "INSERT INTO timbrados (cod, fech_autorizacion, fech_vencimiento) VALUES ";
+    $sql= "(" . $cod . ", " . $fech_auto . ", " . $fech_venc . ");";
+    modificarBdd($sql);
+    //Redireccionamos
+    header('Location: ../principal.html');
+    exit();
+?>
