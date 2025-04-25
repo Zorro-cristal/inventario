f97316548d8fbfeaff739a58a5e8ccd280045f7b
Alejandro Alvarez <zorro201021@hotmail.com>
2025-04-24 23:19:11 -0300

    Estructura de datos para Formulario de proveedor hecha

--- inventario/src/models/proveedor.ts
+++ inventario/src/models/proveedor.ts
@@ -1,6 +1,17 @@
+import { camposForm } from "../views/Formulario";
+
 export interface Proveedor{
     id_proveedor : number;
-    nombre_proveedor : string;
-    telefono_proveedor : number | null;
-    direccion : number | null;
+    nombre : string;
+    telefono? : string;
+    direccion? : string;
+    ruc : string;
 }
+
+export const camposProveedor: Array<camposForm>= [
+    {id: "id_proveedor", requerido: true, ayuda: "", tipo: "number", abrirDialog: null},
+    {id: "nombre", requerido: true, ayuda: "", tipo: "text", abrirDialog: null},
+    {id: "telefono", requerido: false, ayuda: "", tipo: "tel", abrirDialog: null},
+    {id: "direccion", requerido: false, ayuda: "", tipo: "text", abrirDialog: null},
+    {id: "ruc", requerido: true, ayuda: "", tipo: "text", abrirDialog: null}
+];
